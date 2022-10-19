923668460eda634aeeb291cf064b04db282449ba
Oliver Forral <intrepion@gmail.com>
2022-10-19 15:04:22 -0600

    Revert the changes we have just made:

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
         $greet = '';
-        if ($name) {
+        if ($name = $request->query->get('hello')) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
 
